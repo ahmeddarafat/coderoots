f6a00fc905c4ea1ab39b7c890c5a3adc0b1bf6f3
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2024-03-24 02:12:12 +0200

    init app

---
+++ android/app/src/main/kotlin/com/example/coderoots/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coderoots
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:coderoots/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+

b6f160bd24a2aa650615a5ef6bdcdadf04f787a5
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2024-03-24 02:36:42 +0200

    feat: build core service locator

---
+++ lib/core/service_locator/service_locator.dart
@@ -0,0 +1,27 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../cache/app_prefs.dart';
+import '../network/api_service.dart';
+
+import '../network/network_info.dart';
+
+final GetIt getIt = GetIt.instance;
+
+Future<void> initServiceLocator() async {
+  /// Shared prefs
+  final sharedPrefs = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton(
+    () => AppPrefs(sharedPrefs),
+  );
+
+  /// api service
+  getIt.registerLazySingleton(() => DioService());
+
+  /// network checker
+  getIt.registerLazySingleton(
+    () => NetworkInfo(
+      InternetConnectionChecker(),
+    ),
+  );
+}
